9a4a6fcab3131d04ed2fcdf3e482a159f3e564d1
HenrikUd <henrikudam@gmail.com>
2024-07-27 16:14:17 +0300

    vite .env workaround

--- MammothFrontEnd/vite.config.ts
+++ MammothFrontEnd/vite.config.ts
@@ -1,33 +1,40 @@
-import 'dotenv/config';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'tsx',
-  },
-  root: './',
-  build: {
-    outDir: 'build',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-        '.ts': 'tsx',
+export default ({ mode }) => {
+  // Load environment variables based on mode (development or production)
+  const env = loadEnv(mode, process.cwd());
+
+  // Update process.env with loaded variables
+  Object.assign(process.env, env);
+
+  return defineConfig({
+    plugins: [react()],
+    esbuild: {
+      loader: 'tsx',
+    },
+    root: './',
+    build: {
+      outDir: 'build',
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+          '.ts': 'tsx',
+        },
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8082',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL || 'http://localhost:8082',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-});
+  });
+};
