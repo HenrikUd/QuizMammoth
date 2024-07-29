e25145695c0ff3ea6b27530994931bf4797be2a1
HenrikUd <henrikudam@gmail.com>
2024-07-29 18:47:16 +0300

    cookies fix 2

--- MammothBackEnd/api/index.js
+++ MammothBackEnd/api/index.js
@@ -42,6 +42,15 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://quiz-mammoth.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://mammothbackend.vercel.app//');
+
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 
 
 app.use(cors(corsOptions));
